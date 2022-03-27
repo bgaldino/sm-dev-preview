ad71ed3009a556308af75e0335c342a130c71546
Brian Galdino <bgaldino@salesforce.com>
2022-03-27 14:38:05 -0700

    Add color for echo output

--- scripts/assign-permsets.sh
+++ scripts/assign-permsets.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+function echo_attention() {
+  local green='\033[0;32m'
+  local no_color='\033[0m'
+  echo "${green}$1${no_color}"
+}
+
 defaultDir="../sm/main";
 sfdx force:source:deploy -p $defaultDir/default/classes/MockAdapter.cls --apiversion=54.0
 sfdx force:source:deploy -p $defaultDir/default/classes/SalesforceValidationException.cls --apiversion=54.0
@@ -35,3 +41,10 @@ sfdx force:user:permset:assign --permsetname TaxApp_API_Admin
 sfdx force:user:permset:assign --permsetname TaxApp_API_User
 sfdx force:user:permset:assign --permsetname ActivateOrders
 sfdx force:user:permset:assign --permsetname CreateCancellationOrder
+
+sfdx force:source:deploy -p $defaultDir/default/namedCredentials/Salesforce.namedCredential-meta.xml --apiversion=54.0
+
+ncid=`sfdx force:data:soql:query -q "SELECT Id FROM NamedCredential WHERE MasterLabel='Salesforce' LIMIT 1" -r csv | tail -n +2`
+echo_attention ncid=$ncid
+
+sfdx force:source:deploy -p $defaultDir/default/paymentGatewayProviders/SalesforceGatewayProvider.paymentGatewayProvider-meta.xml --apiversion=54.0
