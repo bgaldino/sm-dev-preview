f491d0f177c75994506172cd1681946a0c658a16
Brian Galdino <bgaldino@salesforce.com>
2022-03-27 14:40:53 -0700

    Add color to echo

--- scripts/assign-permsets.sh
+++ scripts/assign-permsets.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+function echo_attention() {
+  local green='\033[0;32m'
+  local no_color='\033[0m'
+  echo "${green}$1${no_color}"
+}
+
 defaultDir="../sm/main";
 sfdx force:source:deploy -p $defaultDir/default/settings/Order.settings-meta.xml --apiversion=55.0
 sfdx force:source:deploy -p $defaultDir/default/settings/Quote.settings-meta.xml --apiversion=55.0
